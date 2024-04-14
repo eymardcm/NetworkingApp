b1437c7095bbcd00f5294a2edbe7b017082f8f6f
Chad Eymard <eymardcm@gmail.com>
2024-04-14 16:27:33 -0400

    Initial Commit

---
+++ NetworkingApp/NetworkingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingAppApp.swift
+//  NetworkingApp
+//
+//  Created by Chad Eymard on 4/14/24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
